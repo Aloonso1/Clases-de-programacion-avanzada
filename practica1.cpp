dfbca5bd4a574251b39de0b2870548baaec98815
Aloonso1 <36949045+Aloonso1@users.noreply.github.com>
2020-10-25 16:21:39 -0600

    Update practica1.cpp
    acabbe la practica

--- practica1.cpp
+++ practica1.cpp
@@ -1,11 +1,11 @@
 //
-// Created by alonso on 14/10/20.
+// Created by alonso on 25/10/20.
 //
 
 #include <stdio.h>
 
 #include <stdlib.h>
- //#include <conio.h>
+
 
 int variable = 0;
 
@@ -13,11 +13,13 @@ int main() {
   //	Variables
   //  Programa
   int op;
-int x=0, y=0, i=0, j=0;
+  // Ejercicios
+  int x = 0, y = 0, i = 0, j = 0;
   while (op != 10) {
     printf("\n");
+    printf("\n");
     printf("PRACTICA 1\n");
-    printf("1.- Mostrar numeros pares e impares del 1 al n \n");
+    printf("1.- Ordenamiento descendiente \n");
     printf("2.- Tabla de multiplicar \n");
     printf("3.- Mayor y menors\n");
     printf("4.- Encontrar si es la suma de otro\n");
@@ -28,7 +30,7 @@ int x=0, y=0, i=0, j=0;
     scanf("%d", & op);
     switch (op) {
     case 1: {
-      printf("Ingrese los numeros a sacar la media");
+    
       printf("Cuantos numeros desea ingresar? \n");
       int temp;
       int d = 0;
@@ -39,14 +41,12 @@ int x=0, y=0, i=0, j=0;
         scanf("%d", & arreglo[i]);
 
       }
-      float suma = 0;
+
       printf(" Los numeros dea cuerdo asu posicion son:\n | ");
       for (i = 0; i < d; i++) {
         printf("%d | ", arreglo[i]);
       }
-      //	for(int i=0; i<d; i++){
-      //			printf("El valor en la posicion %d es: %d \n\n", i+1, arreglo[i]);
-      //	}
+
 
       /* Ordenamos los números del vector vNumeros por el método de burbuja */
       for (i = 0; i < (d - 1); i++) {
@@ -66,23 +66,108 @@ int x=0, y=0, i=0, j=0;
       }
       break;
     }
-    case 2:{
-    	
+    case 2: {
+      int acumulador, contador, primermulti, segunmulti;
+      printf("Primer numero \n ");
+      scanf("%d", & segunmulti);
+      printf("Segundo numero \n ");
+      scanf("%d", & primermulti);
+      if (primermulti >= 0 && segunmulti >= 0) {
+        acumulador = 0;
+
+        if (segunmulti != 0) {
+          contador = 1;
+          while (contador <= primermulti) {
+            acumulador += segunmulti;
+            contador++;
+            printf("\n    %d * %d = %d", segunmulti, (contador-1), acumulador);
+          }
+        }
+
+      } else
+        printf("No estan contemplados los negativos.");
+      break;
+    }
+    break;
+    case 3: {
+
+      printf("Cuantos numeros desea ingresar? \n");
+      int temp;
+      int d = 0;
+      scanf("%d", & d);
+      int arreglo[d];
+      for (int i = 0; i < d; i++) {
+        printf("Ingrese el numero %d/%d= \n", i + 1, d);
+        scanf("%d", & arreglo[i]);
+
+      }
+
+
+
+      /* Ordenamos los números del vector vNumeros por el método de burbuja */
+      for (i = 0; i < (d - 1); i++) {
+        for (j = i + 1; j < d; j++) {
+          if (arreglo[j] > arreglo[i]) {
+            temp = arreglo[j];
+            arreglo[j] = arreglo[i];
+            arreglo[i] = temp;
+          }
+        }
+      }
+
+      /* Mostramos los números ordenados */
+      printf("\n El numero mayor es:\n | ");
+
+      printf("%d | ", arreglo[0]);
+      printf("\n El numero menor es:\n | ");
+
+      printf("%d | ", arreglo[(d - 1)]);
+      break;
+
+    }
+    break;
+    case 4:{
+      int primer, segundo, tercero;
+      printf("Primer numero \n ");
+      scanf("%d", & primer);
+      printf("Segundo numero \n ");
+      scanf("%d", & segundo);
+      printf("Tercero numero \n ");
+      scanf("%d", & tercero);
+      if((primer== segundo+tercero)|(segundo==primer+tercero)|(tercero==primer+segundo )){
+      	printf( "Un numero es la suma de los otros dos");
+	  }else
+	  printf( "No es la suma de ninguno");
 		break;
 	}
       break;
-    case 3:
+    case 5:{
+    	 int cociente, dividendo, divisor, resto;
+		printf( "Primer numero \n " );
+                scanf( "%d", &dividendo );
+                printf( "Segundo numero \n " );
+                scanf( "%d", &divisor );
+
+                if ( dividendo > 0 && divisor > 0 )
+                {
+                    cociente = 0;
+                    resto = dividendo;
+
+                    while ( resto >= divisor )
+                    {
+                        resto -= divisor;
+                        cociente++;
+                        printf( "\n    %d",  resto );
+                    }
+
+                    printf( "\n   %d div %d = %d ( Residuo = %d )", dividendo, divisor, cociente, resto );
+                }
+                else
+                    printf( "No estan contemplados los negativos.");
+                break;
+	}
       break;
-    case 4:
-      break;
-    case 5:
-      break;
-    case 6:
-      break;
-    case 7:
-      break;
-    case 8:
-      break;
+
     case 10:
       break;
     }
